6bc8f4aa30b7057508456f5ad88bf8d49d2a4ebe
Tom Porter <thomas.porter@acm.org>
2013-07-22 16:22:26 -0400

    Third try July - Aug 2013

--- WEEK01/t.rb
+++
@@ -1,9 +0,0 @@
-def mystery_sequence(start1, start2, limit=4)
-  yield start1
-  yield start2
-  nextval = start1 + start2
-  1.upto(limit) do
-    yield nextval
-    nextval, start2 = nextval + start2, nextval
-  end
-end
